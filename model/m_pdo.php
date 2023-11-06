17bf594747f79f4eb80bafa1a1c8d02cef6c01f7
dtrinhitweb <dtrinhit04@gmail.com>
2023-11-06 20:58:42 +0700

    connect db

--- model/m_pdo.php
+++ model/m_pdo.php
@@ -3,12 +3,13 @@
  * Mở kết nối đến CSDL sử dụng PDO
  */
 function pdo_get_connection(){
-    $dburl = "mysql:host=localhost;dbname=hcmclib;charset=utf8";
+    $dburl = "mysql:host=localhost;dbname=duan1;charset=utf8";
     $username = 'root';
     $password = '';
 
     $conn = new PDO($dburl, $username, $password);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "thành công ";
     return $conn;
 }
 /**
