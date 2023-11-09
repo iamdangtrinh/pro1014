e7c8ee6bf8dbdcd74cc3bc64c06daf063ec421f0
nnqn15 <nnqn15@gmail.com>
2023-11-09 13:37:45 +0700

    Create v_admin_user.php

---
+++ view/v_admin_user.php
@@ -0,0 +1,27 @@
+            <h1>Tài khoản</h1>
+            <!-- Recent Orders Table -->
+            <div class="recent-orders">
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Ảnh</th>
+                            <th>Họ Tên</th>
+                            <th>Email</th>
+                            <th>Số điện thoại</th>
+                            <th>Quyền</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td><img src="<?= $base_url ?>upload/blog/author.jpg" class="rounded-3" style="width: 32px; height: 32px;" alt=""></td>
+                            <td class="text-start">Nguyễn Ngọc Quỳnh Nhi</td>
+                            <td>nnqn15@gmail.com</td>
+                            <td>0706903022</td>
+                            <td>Khách hàng</td>
+                            <a href=""><td class="warning">Sửa</td></a>
+                            <a href=""><td class="danger">Xóa</td></a>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+            <!-- End of Recent Orders -->
