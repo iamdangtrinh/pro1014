f7f518ef0919f93f77557a9d9d97c9c50514ef80
Đăng Trình <dtrinhit04@gmail.com>
2023-11-14 16:28:10 +0700

    Update c_user.php

--- controller/c_user.php
+++ controller/c_user.php
@@ -2,9 +2,14 @@
 if (isset($_GET['act'])) {
     switch ($_GET['act']) {
         case 'dashboard':
-            //lay du lieu
-            // hien thi du lieu
-            $view_name = 'user_dashboard';
+            // Nếu tồn tại người dùng thì cho vào dashboard
+            if (isset($_SESSION['user'])) {
+                $view_name = 'user_dashboard';
+            } else {
+                // Neus không tồn tại người dùng thì chuyển về trang đăng ký
+                $_SESSION['error']['register'] = 'Vui lòng đăng ký để truy cập vào trang dashboard';
+                header('location: ' . $base_url . 'user/register');
+            }
             break;
         // Đăng nhập tài khoản
         case 'login':
@@ -26,7 +31,7 @@ if (isset($_GET['act'])) {
                         $_SESSION['user'] = $has_account;
                         // echo "Đăng nhập thành công";     
                         sleep(0.3);
-                        if($_SESSION['user']['VaiTro'] == 1) {
+                        if ($_SESSION['user']['VaiTro'] == 1) {
                             header('location: ' . $base_url . 'admin/dashboard');
                         } else {
                             header('location: ' . $base_url . 'page/home');
@@ -38,32 +43,33 @@ if (isset($_GET['act'])) {
                     }
                 }
             }
+
             $view_name = 'user_login';
             $title = "Đăng nhập tài khoản";
-        break;
+            break;
         // Đăng ký tài khoản
         case 'register':
             //lay du lieu
             include_once 'model/m_user.php';
             // Kiểm tra tài khoản có tồn tại hay không
-            if(isset($_POST['btn_register']) && $_POST['btn_register']) {
+            if (isset($_POST['btn_register']) && $_POST['btn_register']) {
                 $HoTen = $_POST['fullname'];
                 $Email = $_POST['email'];
                 $SoDienThoai = $_POST['number_phone'];
                 $MatKhau = $_POST['password'];
                 $DiaChi = $_POST['address'];
-    
-                if($Email == "" || empty($Email)) {
+
+                if ($Email == "" || empty($Email)) {
                     $_SESSION['error']['register'] = 'Đăng ký không thành công';
-                } else if(!preg_match("/@/", $Email)) {
+                } else if (!preg_match("/@/", $Email)) {
                     $_SESSION['error']['register'] = 'Đăng ký không thành công. Email không hợp lệ';
                 } else {
                     // cho phép đăng ký tài khoản
-                    if(has_email($Email) > 0) {
+                    if (has_email($Email) > 0) {
                         $_SESSION['error']['register'] = 'Đăng ký không thành công. Tài khoản này đã tồn tại';
                     } else {
                         $_SESSION['success']['register'] = 'Đăng ký tài khoản thành công.';
-                        user_add($SoDienThoai,$Email,$HoTen,md5($MatKhau),$DiaChi);
+                        user_add($SoDienThoai, $Email, $HoTen, md5($MatKhau), $DiaChi);
                     }
                 }
             }
@@ -72,9 +78,11 @@ if (isset($_GET['act'])) {
             $title = "Đăng ký tài khoản";
             break;
         case 'forgotPassword':
-            //lay du lieu
-            // hien thi du lieu
-            $view_name = 'user_forgotPassword';
+            if (isset($_SESSION['user'])) {
+                header('location: ' . $base_url . 'page/home');
+            } else {
+                $view_name = 'user_forgotPassword';
+            }
             break;
         case 'logout':
             unset($_SESSION['user']);
