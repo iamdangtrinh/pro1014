50b9a8d7c4af05c0b4d04b15b00e75cb7bda2d1a
trannghia204 <tranhuunghia204@gmail.com>
2023-11-22 12:48:25 +0700

    category_detail

--- model/m_category.php
+++ model/m_category.php
@@ -3,6 +3,8 @@
     function category_getALLDM(){
         return pdo_query("SELECT danhmuc.TenDM, danhmuccon.TenDMC FROM danhmuc INNER JOIN danhmuccon ON danhmuc.MaDM = danhmuccon.MaDM ORDER BY danhmuc.MaDM ASC;");
     }
-
+    function category_getbyDM($MaDM, $MaDMC=""){
+        return pdo_query("SELECT * FROM sanpham sp INNER JOIN danhmuc dm ON sp.MaDM = dm.MaDM INNER JOIN danhmuccon dmc ON dm.MaDM = dmc.MaDM WHERE dm.MaDM = ? AND dmc.MaDMC = ?", $MaDM, $MaDMC);
+    }
     
 ?>
