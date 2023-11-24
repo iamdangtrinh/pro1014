c63a78d0ce9fe8e616a5a98eb7778a88591bbf85
nguyenphongphu1234 <134608320+PhuNguyenPhong5524@users.noreply.github.com>
2023-11-24 13:32:22 +0700

    Update v_admin_user.php

--- view/v_admin_user.php
+++ view/v_admin_user.php
@@ -1,35 +1,49 @@
-<style>
-    .recent-orders{
-        border-radius: 10px;
-      
-    }
-    table{
-        font-size: 15px;
-        text-align: center;
-        border-collapse: collapse;
-        
-      
-      
-    }
-    table thead{
-        background: #ffc0cba8;
-       
-        
-    }
-    table thead tr .chon{
-        border-radius: 20px 0 0 0 ;
-        width: 100px;
-        
-    }
-    table thead tr th, table tbody tr td{
-        border-bottom: 3px solid pink;
-        text-align: center;
-    }
 
-    .row {
-        display: none;
-    }
-    .lenh{
+<style>
+:root{
+    --box-shadow: 0 2rem 3rem var(--color-light);
+}
+
+.row{
+    display: none;
+}
+.box{
+    margin-top: 10px;
+}
+h1{
+    margin-top: 10px;
+}
+table {
+    width: 100%;
+    border-collapse: collapse;
+    box-shadow: var(--box-shadow);
+    border-radius: 10px;
+}
+table:hover {
+    box-shadow: 5px 5px 5px #ffd5dd;
+    
+}
+
+thead {
+    background-color: #ffc0cba8;
+    color: white;
+    font-size: 14px;
+    
+}
+tbody{
+    border-radius: 20px;
+}
+th, td {
+    padding: 15px;
+    text-align: left;
+    text-align: center;
+    font-weight: 600;
+    font-size: 13px;
+}
+td{
+    background-color: #ffc0cb33;
+}
+.lenh{
         display: flex;
         text-align: center;
         justify-content: center;
@@ -41,16 +55,65 @@
         padding: 5px;
         font-size: 16px;
     }
+    .sua a{
+        border: 1px solid #ffff;
+        background-color: #ffc0cba8;
+        border-radius: 5px;
+    }
     .sua a:hover{
-        background: #f8d7da;
+        background: #6c9bcf;
         color: #fff;
         border-radius: 5px;
+        border: 1px solid #ffff;
+        color: #ffff;
+
     }
-  
-    .xoa a:hover{
-        background: #f8d7da;
+     .xoa a{
+        border: 1px solid #ffff;
+        background-color: #ffc0cba8;
         border-radius: 5px;
+     }
+    .xoa a:hover{
+        background: #ba5a87;
+        border-radius: 5px;
+        border: 1px solid #ffff;
+        color: #ffff;
+
     }
+    .box_button{
+        display: flex;
+    }
+    .them{
+
+       margin-top: 20px;
+       padding: 10px;
+       font-size: 16px;
+       margin: 5px;
+       background: pink;
+       border-radius: 5px;
+       color: #ffff;
+    }
+    .them:hover{
+        box-shadow: 5px 5px 5px #ffd5dd;
+        color: #fff;
+        background: pink;
+        
+    }
+    .remove{
+
+        padding: 10px;
+        font-size: 16px;
+        margin: 5px;
+        background: pink;
+        border-radius: 5px;
+        color: #ffff;
+        }
+        .remove:hover{
+        box-shadow: 5px 5px 5px #ffd5dd;
+        color: #fff;
+        background: pink;
+        
+        }
 </style>
             <h1>Tài khoản</h1>
             <!-- Recent Orders Table -->
@@ -63,7 +126,7 @@
                             <th>Email</th>
                             <th>Số điện thoại</th>
                             <th>Quyền</th>
-                          
+                            <th style="text-align: center;">Lệnh</th>
                         </tr>
                     </thead>
                     <tbody>
@@ -90,10 +153,10 @@
                                     ?>
                                 </td>
                                 <td >
-                                    <div class="lenh">
-                                        <div class="sua" ><a href="" style="font-weight: 600; color: #6c9bcf;">Sửa</a></div>
-                                        <div class="xoa"><a href=""  style="font-weight: 600; color: #ba5a87;">Xóa</a></div>
-                                    </div>
+                                <div class="lenh">
+                                    <div class="sua" ><a href="" style="font-weight: 600; ">Sửa</a></div>
+                                    <div class="xoa"><a href=""  style="font-weight: 600; ">Xóa</a></div>
+                                </div>
                                 </td>
 
                             </tr>
@@ -102,6 +165,7 @@
                 </table>
             </div>
             <!-- End of Recent Orders -->
+
 
 
 
