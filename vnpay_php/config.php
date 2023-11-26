7c6eb9fab86dc3b23de1c169e255a1d2f2e200e4
nnqn15 <nnqn15@gmail.com>
2023-11-26 23:34:51 +0700

    tích hợp vn pay( cần fig)

---
+++ vnpay_php/config.php
@@ -0,0 +1,17 @@
+<?php
+date_default_timezone_set('Asia/Ho_Chi_Minh');
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+$vnp_TmnCode = "NW73VSxx"; //Mã định danh merchant kết nối (Terminal Id)
+$vnp_HashSecret = "XXGIASNRZCLTQVKVWAIEWCPDEYZSFSxx"; //Secret key
+$vnp_Url = "https://sandbox.vnpayment.vn/paymentv2/vpcpay.html";
+$vnp_Returnurl = "http://localhost/pro1041/vnpay_php/vnpay_return.php";
+$vnp_apiUrl = "http://sandbox.vnpayment.vn/merchant_webapi/merchant.html";
+$apiUrl = "https://sandbox.vnpayment.vn/merchant_webapi/api/transaction";
+//Config input format
+//Expire
+$startTime = date("YmdHis");
+$expire = date('YmdHis',strtotime('+15 minutes',strtotime($startTime)));
