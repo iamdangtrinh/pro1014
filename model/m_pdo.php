001eab9e490a394d7957f6faff7297bdc0d7874e
Đăng Trình <dtrinhit04@gmail.com>
2023-11-28 16:13:39 +0700

    Update m_pdo.php

--- model/m_pdo.php
+++ model/m_pdo.php
@@ -2,15 +2,23 @@
 /**
  * Mở kết nối đến CSDL sử dụng PDO
  */
-function pdo_get_connection(){
-    $host = '192.46.229.88';
-    $port = '3306';
-    $dburl = "mysql:host=$host;port=$port;dbname=pro1014;charset=utf8";
-    $username = 'pro1014';
-    $password = 's8LBB8J5rLNsyJ3K';
-    $conn = new PDO($dburl, $username, $password);
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    return $conn;
+function pdo_get_connection() {
+        $host = '192.46.229.88';
+        $port = '3306';
+        $dburl = "mysql:host=$host;port=$port;dbname=pro1014;charset=utf8";
+        $username = 'pro1014';
+        $password = 's8LBB8J5rLNsyJ3K';
+        try{
+            $conn = new PDO($dburl, $username, $password);
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        return $conn;
+        }
+        catch(PDOException) {
+            echo "Kết nối không thành công";
+        }
+        finally {
+            unset($conn);
+        }
 }
 // function pdo_get_connection(){
 //     $host = 'localhost';
@@ -28,17 +36,15 @@ function pdo_get_connection(){
  * @param array $args mảng giá trị cung cấp cho các tham số của $sql
  * @throws PDOException lỗi thực thi câu lệnh
  */
-function pdo_execute($sql){
+function pdo_execute($sql) {
     $sql_args = array_slice(func_get_args(), 1);
-    try{
+    try {
         $conn = pdo_get_connection();
         $stmt = $conn->prepare($sql);
         $stmt->execute($sql_args);
-    }
-    catch(PDOException $e){
+    } catch (PDOException $e) {
         throw $e;
-    }
-    finally{
+    } finally {
         unset($conn);
     }
 }
@@ -49,19 +55,17 @@ function pdo_execute($sql){
  * @return array mảng các bản ghi
  * @throws PDOException lỗi thực thi câu lệnh
  */
-function pdo_query($sql){
+function pdo_query($sql) {
     $sql_args = array_slice(func_get_args(), 1);
-    try{
+    try {
         $conn = pdo_get_connection();
         $stmt = $conn->prepare($sql);
         $stmt->execute($sql_args);
         $rows = $stmt->fetchAll();
         return $rows;
-    }
-    catch(PDOException $e){
+    } catch (PDOException $e) {
         throw $e;
-    }
-    finally{
+    } finally {
         unset($conn);
     }
 }
@@ -72,19 +76,17 @@ function pdo_query($sql){
  * @return array mảng chứa bản ghi
  * @throws PDOException lỗi thực thi câu lệnh
  */
-function pdo_query_one($sql){
+function pdo_query_one($sql) {
     $sql_args = array_slice(func_get_args(), 1);
-    try{
+    try {
         $conn = pdo_get_connection();
         $stmt = $conn->prepare($sql);
         $stmt->execute($sql_args);
         $row = $stmt->fetch(PDO::FETCH_ASSOC);
         return $row;
-    }
-    catch(PDOException $e){
+    } catch (PDOException $e) {
         throw $e;
-    }
-    finally{
+    } finally {
         unset($conn);
     }
 }
@@ -95,32 +97,30 @@ function pdo_query_one($sql){
  * @return giá trị
  * @throws PDOException lỗi thực thi câu lệnh
  */
-function pdo_query_value($sql){
+function pdo_query_value($sql) {
     $sql_args = array_slice(func_get_args(), 1);
-    try{
+    try {
         $conn = pdo_get_connection();
         $stmt = $conn->prepare($sql);
         $stmt->execute($sql_args);
         $row = $stmt->fetch(PDO::FETCH_ASSOC);
         return array_values($row)[0];
-    }
-    catch(PDOException $e){
+    } catch (PDOException $e) {
         throw $e;
-    }
-    finally{
+    } finally {
         unset($conn);
     }
 }
 // up anh
-function upanh($name,$target_dir){
-    $target_file=$target_dir . basename($_FILES[''.$name.'']['name']);
-    if(!file_exists($target_file)){
+function upanh($name, $target_dir) {
+    $target_file = $target_dir.basename($_FILES[''.$name.'']['name']);
+    if(!file_exists($target_file)) {
         move_uploaded_file($_FILES[''.$name.'']['tmp_name'], $target_file);
     }
-    $img=$_FILES[''.$name.'']['name'];
+    $img = $_FILES[''.$name.'']['name'];
     return $img;
 }
-function xoaanh($MaSP){
-    $list=pdo_query_one("SELECT * FROM sanpham WHERE MaSP = ?",$MaSP);
+function xoaanh($MaSP) {
+    $list = pdo_query_one("SELECT * FROM sanpham WHERE MaSP = ?", $MaSP);
     return $list['Hinh'];
 }
