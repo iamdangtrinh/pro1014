e4163b197395af5cb67153977384b40f25fd0ae8
dtrong1208 <trongntdps31006@fpt.edu.vn>
2023-12-11 04:03:45 +0700

    Cập nhật mật khẩu k còn dính md5, up ảnh đại diện thành công

---
+++ .history/controller/c_user_20231211025956.php
@@ -0,0 +1,167 @@
+<?php
+include_once 'model/m_user.php';
+if (isset($_GET['act'])) {
+    switch ($_GET['act']) {
+        case 'dashboard':
+            // Nếu tồn tại người dùng thì cho vào dashboard
+            if (isset($_SESSION['user']['VaiTro'])) {
+                include_once 'model/m_cart.php';
+                $history_cart =  history_cart($_SESSION['user']['MaTK']);
+                $view_name = 'user_dashboard';
+            } else {
+                // Neus không tồn tại người dùng thì chuyển về trang đăng ký
+                $_SESSION['error']['register'] = 'Vui lòng đăng ký để truy cập vào trang dashboard';
+                header('location: ' . $base_url . 'user/register');
+            }
+            // $MaTK = $_SESSION['user']['MaTK'];
+            $info_user = user_getById($_SESSION['user']['MaTK']);
+            $title = 'Quản lí tài khoản';
+            break;
+        // Đăng nhập tài khoản
+        case 'login': 
+            include_once 'model/m_user.php';
+            //lay du lieu
+            if (isset($_POST['btn_login']) && $_POST['btn_login']) {
+                $email = $_POST['email'];
+                $password = $_POST['password'];
+                if (empty($email) || empty($password)) {
+                    // Hiển thị lỗi
+                    $_SESSION['error']['login'] = "Email hoặc mật khẩu không được để trống";
+                } else {
+                    // kiểm tra tài khoản có tồn tại hay không
+                    $has_account = check_login($email, $password);
+                    // cho phép đăng nhập
+                    if ($has_account) {
+                        $_SESSION['user'] = $has_account;
+                        // echo "Đăng nhập thành công";     
+                        if ($_SESSION['user']['VaiTro'] == 0) {
+                            header('location: ' . $base_url . 'page/home');
+                        } else {
+                            header('location: ' . $base_url . 'admin/dashboard');
+                        }
+                        // Chuyển về trang chủ
+                    } else if ($has_account == 0) {
+                        $_SESSION['error']['login'] = "Tài khoản hoặc mật khẩu sai. Vui lòng thử lại";
+                    }
+                }
+            }
+            $view_name = 'user_login';
+            $title = "Đăng nhập tài khoản";
+            break;
+        // Đăng ký tài khoản
+        case 'register':
+            if (isset($_POST['btn_register']) && $_POST['btn_register']) {
+                $HoTen = $_POST['fullname'];
+                $Email = $_POST['email'];
+                $SoDienThoai = $_POST['number_phone'];
+                $MatKhau = $_POST['password'];
+                $DiaChi = $_POST['address'];
+
+                if (empty($SoDienThoai) || empty($MatKhau) || empty($HoTen) || empty($DiaChi)) {
+                    $_SESSION['error']['register'] = 'Đăng ký không thành công. Vui lòng thử lại';
+                    // header('location: ' . $base_url . 'user/register');
+                } else if (empty($Email)) {
+                    $_SESSION['error']['register'] = 'Đăng ký không thành công. Vui lòng thử lại sau';
+                // } else if (true) {
+                //     $_SESSION['error']['register'] = 'Đăng ký không thành công. Email không hợp lệ';
+            // Kiểm tra tài khoản có tồn tại hay không
+                } else if (has_email($Email) > 0) {
+                    $_SESSION['error']['register'] = 'Đăng ký không thành công. Tài khoản này đã tồn tại';
+                } else {
+                    // Allow registration
+                    user_add($SoDienThoai, $Email, $HoTen, $MatKhau, $DiaChi);
+                    $_SESSION['success']['register'] = 'Đăng ký tài khoản thành công.';
+                }
+            }
+            $view_name = 'user_register';
+            $title = "Đăng ký tài khoản";
+            break;
+        case 'update_info':
+            if(isset($_POST['btn_update_info']) && $_POST['btn_update_info']){
+                $info_user = user_getById($_SESSION['user']['MaTK']);
+                $HoTen = $_POST['HoTen'];
+                $SoDienThoai = $_POST['SoDienThoai'];
+                $DiaChi = $_POST['DiaChi'];
+                $HinhAnh = $_POST['Hinh'];
+                $MaTK=$_SESSION['user']['MaTK'];
+                if(empty($HoTen) && empty($SoDienThoai) && empty($DiaChi)){
+                    $_SESSION['loi'] = 'Vui lòng nhập <strong>các thông tin</strong> cho tài khoản!';
+                }elseif(empty($HoTen)){
+                    $_SESSION['loi'] = 'Vui lòng nhập <strong>họ tên</strong> cho tài khoản!';
+                }elseif(empty($SoDienThoai)){
+                    $_SESSION['loi'] = 'Vui lòng nhập <strong>số điện thoại</strong> cho tài khoản!';
+                }elseif(empty($DiaChi)){
+                    $_SESSION['loi'] = 'Vui lòng nhập <strong>địa chỉ</strong> cho tài khoản!';
+                }else{
+                    if(isset($_FILES['upHinh']['name']) && $_FILES['upHinh']['name']!= ""){
+                        $TenHinh = 'upHinh';
+                        $Hinh = user_upanh($TenHinh);
+                        if()
+                        []
+                    }else{
+                        $Hinh = $_POST['Hinh'];
+                    }
+                    user_edit_avatar($MaTK, $HinhAnh);
+                    user_edit_info($MaTK,$SoDienThoai, $HoTen, $DiaChi);
+                    $_SESSION['thanhcong'] = 'Bạn đã cập nhật tài khoản thành công!';
+                }
+            }
+            header('location: '.$base_url.'user/dashboard');
+            break;
+        case 'update_pass':
+            if(isset($_POST['btn_update_pass']) && $_POST['btn_update_pass']){
+                $info_user = user_getById($_SESSION['user']['MaTK']);
+                $MatKhau = $_POST['password'];
+                $MatKhauMoi = $_POST['new_password'];
+                $MatKhauNhapLai = $_POST['re_password'];
+                $MaTK = $_SESSION['user']['MaTK'];
+                if(empty($MatKhau) && empty($MatKhauMoi) && empty($MatKhauNhapLai)){
+                    $_SESSION['loi'] = 'Vui lòng nhập các thông tin cho tài khoản!';
+                }elseif(md5($MatKhau) != $info_user['MatKhau']){
+                    $_SESSION['loi'] = 'Vui lòng nhập đúng mật khẩu cũ của tài khoản!';
+                }elseif($MatKhau == $MatKhauMoi){
+                    $_SESSION['loi'] = 'Vui lòng nhập mật khẩu mới khác mật khẩu cũ cho tài khoản!';
+                }elseif($MatKhauMoi != $MatKhauNhapLai){
+                    $_SESSION['loi'] = 'Vui lòng nhập mật khẩu mới và mật khẩu nhập lại giống nhau!';
+                }elseif(empty($MatKhauMoi)){
+                    $_SESSION['loi'] = 'Vui lòng nhập mật khẩu mới cho tài khoản!';
+                }elseif(empty($MatKhauNhapLai)){
+                    $_SESSION['loi'] = 'Vui lòng nhập lại mật khẩu mới cho tài khoản!';
+                }elseif(empty($MatKhau)){
+                    $_SESSION['loi'] = 'Vui lòng nhập mật khẩu cũ cho tài khoản!';
+                }
+                else{
+                    user_update_pass($MaTK, md5($MatKhauMoi));
+                    $_SESSION['thanhcong'] = 'Bạn đã cập nhật mật khẩu thành công!';
+                }
+            }
+            header('location: '.$base_url.'user/dashboard');
+            break;
+        case 'forgotPassword':
+            if (isset($_SESSION['user'])) {
+                header('location: ' . $base_url . 'page/home');
+            } else {
+                $view_name = 'user_forgotPassword';
+            }
+            $title = "Quên mật khẩu";
+            break;
+        case 'logout':
+            unset($_SESSION['user']);
+            header('location: ' . $base_url . 'page/home');
+            break;
+        case 'history':
+            $MaTK=$_SESSION['user']['MaTK'];
+            $MaHD=$_GET['MaHD'];
+            $chitiethd=getproductByHD($MaTK,$MaHD);
+            $view_name = 'user_history';
+            $title = "Chi tiết đơn hàng";
+            break;
+        default:
+            $view_name = 'page_home';
+            break;
+    }
+    include_once 'view/v_user_layout.php';
+} else {
+    header('location: ' . $base_url . 'page/home');
+}
+?>
---
+++ .history/model/m_user_20231211014032.php
@@ -0,0 +1,66 @@
+<?php 
+    include_once 'm_pdo.php';
+    // kiểm tra tài khoản trong sql có đúng không
+    function check_login($email, $password){
+        return pdo_query_one("SELECT * FROM taikhoan WHERE Email=? AND MatKhau=?",$email,md5($password));
+    }
+    // kiểm tra email có tồn tại hay không
+    function has_email($email){
+        return pdo_query_one("SELECT EMAIL FROM taikhoan WHERE Email=? ",$email);
+    }
+    // Tạo tài khoản người dùng 
+    function user_add($SoDienThoai, $Email, $HoTen, $MatKhau, $DiaChi){
+        pdo_execute("INSERT INTO taikhoan(`SoDienThoai`,`Email`,`HoTen`,`MatKhau`,`DiaChi`) VALUES(?,?,?,?,?)",$SoDienThoai,$Email,$HoTen,md5($MatKhau),$DiaChi);
+    }
+    // Thêm user từ Admin
+    function user_admin_add($SoDienThoai, $Email, $HoTen, $MatKhau, $DiaChi,$VaiTro){
+        pdo_execute("INSERT INTO taikhoan(`SoDienThoai`,`Email`,`HoTen`,`MatKhau`,`DiaChi`,`VaiTro`) VALUES(?,?,?,?,?,?)",$SoDienThoai,$Email,$HoTen,md5($MatKhau),$DiaChi,$VaiTro);
+    }
+    // Lấy thông tin user
+    function user_getAll(){
+        return pdo_query("SELECT * FROM taikhoan");
+    }
+    function user_checkEmail($Email){
+        return pdo_query_one("SELECT * FROM taikhoan WHERE Email=?",$Email);
+    }
+    function user_getById($MaTK){
+        return pdo_query_one("SELECT * FROM taikhoan WHERE MaTK=?",$MaTK);
+    }
+    function user_edit($MaTK,$SoDienThoai, $Email, $HoTen, $MatKhau, $DiaChi,$VaiTro){
+        pdo_execute("UPDATE taikhoan SET SoDienThoai=?,Email=?,HoTen=?,MatKhau=?,DiaChi=?,VaiTro=? WHERE MaTK=?",
+        $SoDienThoai, $Email, $HoTen, $MatKhau, $DiaChi,$VaiTro,$MaTK);
+    }
+    function user_delete($MaTK){
+        pdo_execute("DELETE FROM taikhoan WHERE MaTK=?",$MaTK);
+    }
+    function user_countAll(){
+        return pdo_query_value("SELECT COUNT(*) FROM taikhoan");
+    }
+    function user_update_info($HoTen, $SoDienThoai, $DiaChi, $MaTK){
+        return pdo_execute("UPDATE taikhoan SET HoTen=?, SoDienThoai =?, DiaChi=? WHERE MaTK=?", $HoTen,$SoDienThoai, $DiaChi, $MaTK);
+    }
+    function user_edit_info($MaTK,$SoDienThoai, $HoTen, $DiaChi){
+        pdo_execute("UPDATE taikhoan SET SoDienThoai=?,HoTen=?,DiaChi=? WHERE MaTK=?",
+        $SoDienThoai, $HoTen, $DiaChi, $MaTK);
+    }
+    function user_edit_avatar($MaTK,$HinhAnh){
+        pdo_execute("UPDATE taikhoan SET Hinh WHERE MaTK=?",
+        $SoDienThoai, $HoTen, $DiaChi, $MaTK);
+    }
+    function user_update_pass($MaTK, $MatKhau){
+        pdo_execute("UPDATE taikhoan SET MatKhau = ? WHERE MaTK = ?", $MatKhau, $MaTK);
+    }
+    //up anh
+    function user_upanh($name){
+        $target_dir='upload/avatar/';
+        $target_file=$target_dir . basename($_FILES[''.$name.'']['name']);
+        if(!file_exists($target_file)){
+            move_uploaded_file($_FILES[''.$name.'']['tmp_name'], $target_file);
+        }
+        $img=basename($_FILES[''.$name.'']['name']);
+        return $img;
+    }
+    function user_timhinhxoaanh($MaTK){
+        return pdo_query_one("SELECT * FROM taikhoan WHERE MaTK = ?",$MaTK);
+    }
+?>
