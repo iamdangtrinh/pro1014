f947274290c77d1da6b3d8f1ecba8cdf2c732088
Đăng Trình <dtrinhit04@gmail.com>
2023-12-10 12:31:07 +0700

    Update config.php

--- config.php
+++ config.php
@@ -4,6 +4,7 @@
     $view_name='';
     $title ="Trang chủ";
     $base_url='http://localhost/pro1014/';
+    // $base_url='https://laptopgiare.click/';
 
     define("IMG_PRODUCT","upload/uploadSP/");
     define("IMG_PRODUCT_ADMIN","../upload/uploadSP/");
