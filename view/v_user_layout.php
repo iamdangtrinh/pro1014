4c19d097081d3b1cffdaf8d61cc585451419258e
Đăng Trình <dtrinhit04@gmail.com>
2023-12-10 23:30:32 +0700

    Update v_user_layout.php

--- view/v_user_layout.php
+++ view/v_user_layout.php
@@ -563,6 +563,37 @@
 
     <a id="scroll-top" href="#top" title="Top" role="button"><i class="fa-solid fa-arrow-up"></i></a>
 
+    <!-- Messenger Plugin chat Code -->
+    <div id="fb-root"></div>
+
+    <!-- Your Plugin chat code -->
+    <div id="fb-customer-chat" class="fb-customerchat">
+    </div>
+
+    <script>
+      var chatbox = document.getElementById('fb-customer-chat');
+      chatbox.setAttribute("page_id", "152578301281395");
+      chatbox.setAttribute("attribution", "biz_inbox");
+    </script>
+
+    <!-- Your SDK code -->
+    <script>
+      window.fbAsyncInit = function() {
+        FB.init({
+          xfbml            : true,
+          version          : 'v18.0'
+        });
+      };
+
+      (function(d, s, id) {
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) return;
+        js = d.createElement(s); js.id = id;
+        js.src = 'https://connect.facebook.net/vi_VN/sdk/xfbml.customerchat.js';
+        fjs.parentNode.insertBefore(js, fjs);
+      }(document, 'script', 'facebook-jssdk'));
+    </script>
+
     <!-- Plugins JS File -->
     <script src="<?= $base_url ?>assets/js/bootstrap.bundle.min.js"></script>
     <script src="<?= $base_url ?>assets/js/plugins.min.js"></script>
